fd8e947f108557ffdc95b1d304dcff5f4530e952
aswin-0404 <aswinofficial101@gmail.com>
2026-02-25 14:09:42 +0530

    razorpay removed

--- payment/urls.py
+++ payment/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from .views import CreatePaymentOrder,VerifyPayment
 
 urlpatterns = [
-    path('payments/create-order/',CreatePaymentOrder.as_view()),
-    path ('payments/verify/',VerifyPayment.as_view()),
+    # path('payments/create-order/',CreatePaymentOrder.as_view()),
+    # path ('payments/verify/',VerifyPayment.as_view()),
 ]
