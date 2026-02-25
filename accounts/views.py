4b15d877a60cfdb750038ddfa3cba2bad484a3c4
aswin-0404 <aswinofficial101@gmail.com>
2026-02-25 17:19:42 +0530

    debugging regisister removed smtp

--- accounts/views.py
+++ accounts/views.py
@@ -11,7 +11,7 @@ from rest_framework.permissions import AllowAny,IsAuthenticated
 from django.contrib.auth.tokens import default_token_generator
 from django.utils.http import urlsafe_base64_encode
 from django.utils.encoding import force_bytes
-# from django.core.mail import send_mail
+from django.core.mail import send_mail
 from django.utils.http import urlsafe_base64_decode
 # Create your views here.
 
